30d24a02d72af9fac7c97652024d361fa4f90d8b
gmganesh3110 <122125196+gmganesh3110@users.noreply.github.com>
2023-01-09 00:06:10 +0530

    header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route ,Routes} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
       <Route exact path='/' element={<Homepage />} />
       <Route exact path='/shop' element={<ShopPage />} />
