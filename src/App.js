6404d8f73177d58030cadeed96ceffe719cb5925
gmganesh3110 <122125196+gmganesh3110@users.noreply.github.com>
2023-01-08 18:26:23 +0530

    router section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route ,Routes} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage';
+
+const HatsPage=()=>(
+  <div>
+    <h1>hatsPage</h1>
+  </div>
+)
+
 function App() {
   return (
     <div >
-      <Homepage ></Homepage>
+      {/* <Route path='/'  element={<Homepage />} ></Route> */}
+      <Routes>
+      <Route exact path='/' element={<Homepage />} />
+      <Route exact path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-items/menuItem.jsx
+++ src/components/menu-items/menuItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import './menuItem.css'
-
-const MenuItem=({title,imageUrl,size})=>(
+import { useNavigate } from "react-router-dom";
+const MenuItem=({title,imageUrl,size,linkUrl})=>{
+    const navigate = useNavigate();
+    return(
           <div
-           className={`${size} menu-item`}>
+           className={`${size} menu-item`} 
+           onClick={()=> navigate(linkUrl)}
+           >
             <div className="background-image"  style={{backgroundImage:`url(${imageUrl})`}} > 
             </div>
         <div className="content">
@@ -11,6 +15,7 @@ const MenuItem=({title,imageUrl,size})=>(
             <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-)
+    )
+}
 
 export default MenuItem
